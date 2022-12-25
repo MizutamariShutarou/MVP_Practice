a5ec7ee6c99524ebba008af01aa80bb3cd7444e1
MizutamariShutaro <shutaro0415@outlook.jp>
2022-12-25 23:13:12 +0900

    [add] コミット

---
+++ Assets/InGameParamator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class InGameParamator : MonoBehaviour
+{
+    private int _score;
+    private float _time;
+
+    public int Score => _score;
+    private float Time => _time;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
